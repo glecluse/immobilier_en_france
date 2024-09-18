e061e3d2eaf8f5470a745839a6048502de698332
glecluse <geoffrey.lecluse45@gmail.com>
2024-09-18 20:18:36 +0200

    maj du 18/09

--- src/views/conclusion.py
+++ src/views/conclusion.py
@@ -1,28 +1,38 @@
 import streamlit as st
 
 def load_view():
-    st.title("Conclusion de l'Analyse du Marché Immobilier en France")
+    st.title("Conclusion de l'analyse du marché immobilier en France")
 
-    st.write("""
-    Les graphiques interactifs de cette application ont permis de visualiser des tendances et des corrélations importantes entre les transactions immobilières et les taux d'intérêt en France de 2015 à 2023. Voici les principales conclusions tirées de ces analyses :
+    # Créer des colonnes : texte à gauche et image à droite
+    col1, col2 = st.columns([2, 1])  # La colonne de gauche est plus large pour le texte
 
-    #### Corrélation entre le Nombre de Transactions et les Taux d'Intérêt
-    Sur toute la période analysée, il existe une corrélation notable entre le nombre de transactions immobilières et les taux d'intérêt. Cela indique que les fluctuations des taux d'intérêt ont un impact significatif sur le volume des transactions immobilières. Une baisse des taux d'intérêt tend à stimuler le marché, augmentant le nombre de transactions, tandis qu'une hausse des taux d'intérêt semble freiner l'activité transactionnelle.
+    # Texte à gauche
+    with col1:
+        st.markdown("""
+        Les graphiques interactifs de cette application ont permis de visualiser des tendances et des corrélations importantes entre les transactions immobilières et les taux d'intérêt en France de 2015 à 2023. Voici les principales conclusions tirées de ces analyses :
 
-    #### Corrélation entre le Nombre de Transactions et le Montant Moyen des Transactions
-    Les données montrent une corrélation entre le nombre de transactions et le montant moyen des transactions jusqu'à la fin de l'année 2022. Cela suggère qu'une augmentation du volume des transactions est généralement associée à une augmentation des prix des transactions immobilières. Cependant, à partir de 2023, cette corrélation s'est rompue, indiquant une décorrélation entre ces deux variables jusque 2024. Cette rupture pourrait être due à divers facteurs économiques ou réglementaires, nécessitant une analyse plus approfondie pour identifier les causes sous-jacentes.
+        #### Corrélation entre le nombre de transactions et les taux d'intérêt
+        Sur toute la période analysée, il existe une corrélation notable entre le nombre de transactions immobilières et les taux d'intérêt. Cela indique que les fluctuations des taux d'intérêt ont un impact significatif sur le volume des transactions immobilières. Une baisse des taux d'intérêt tend à stimuler le marché, augmentant le nombre de transactions, tandis qu'une hausse des taux d'intérêt semble freiner l'activité transactionnelle.
 
-    #### Corrélation entre les Taux d'Intérêt et le Montant Moyen des Transactions
-    De manière similaire, une corrélation est observée entre les taux d'intérêt et le montant moyen des transactions jusqu'à la fin de l'année 2022. Les taux d'intérêt plus bas sont souvent associés à des montants de transaction plus élevés, ce qui peut être attribué à un pouvoir d'achat accru des emprunteurs. Cependant, cette relation a également montré une décorrélation à partir de 2023, suggérant que d'autres facteurs ont commencé à influencer les prix des transactions indépendamment des taux d'intérêt.
+        #### Corrélation entre le nombre de transactions et le montant moyen des transactions
+        Les données montrent une corrélation entre le nombre de transactions et le montant moyen des transactions jusqu'à la fin de l'année 2022. Cela suggère qu'une augmentation du volume des transactions est généralement associée à une augmentation des prix des transactions immobilières. Cependant, à partir de 2023, cette corrélation s'est rompue, indiquant une décorrélation entre ces deux variables jusqu'en 2024. Cette rupture pourrait être due à divers facteurs économiques ou réglementaires, nécessitant une analyse plus approfondie pour identifier les causes sous-jacentes.
 
-    #### Implications et Recommandations
-    Les résultats de cette analyse soulignent l'importance des taux d'intérêt comme facteur clé influençant le marché immobilier en France. La corrélation persistante entre les taux d'intérêt et le nombre de transactions suggère que les décideurs politiques et les investisseurs doivent surveiller attentivement les politiques monétaires et les conditions de prêt.
+        #### Corrélation entre les taux d'intérêt et le montant moyen des transactions
+        De manière similaire, une corrélation est observée entre les taux d'intérêt et le montant moyen des transactions jusqu'à la fin de l'année 2022. Les taux d'intérêt plus bas sont souvent associés à des montants de transaction plus élevés, ce qui peut être attribué à un pouvoir d'achat accru des emprunteurs. Cependant, cette relation a également montré une décorrélation à partir de 2023, suggérant que d'autres facteurs ont commencé à influencer les prix des transactions indépendamment des taux d'intérêt.
 
-    La rupture des corrélations observées à partir de 2023 entre le nombre de transactions et le montant moyen des transactions, ainsi qu'entre les taux d'intérêt et le montant moyen des transactions, indique des changements dans le marché qui méritent une attention particulière. Ces changements pourraient être dus à des facteurs macroéconomiques, des modifications de la réglementation immobilière, ou des variations dans l'offre et la demande de logements.
+        #### Implications et recommandations
+        Les résultats de cette analyse soulignent l'importance des taux d'intérêt comme facteur clé influençant le marché immobilier en France. La corrélation persistante entre les taux d'intérêt et le nombre de transactions suggère que les décideurs politiques et les investisseurs doivent surveiller attentivement les politiques monétaires et les conditions de prêt.
 
-    Pour les utilisateurs de cette application, il est recommandé de continuer à surveiller ces tendances et d'approfondir l'analyse pour comprendre les dynamiques sous-jacentes qui influencent le marché immobilier français. En utilisant cette application, vous disposez d'un outil puissant pour explorer les données et prendre des décisions informées.
+        La rupture des corrélations observées à partir de 2023 entre le nombre de transactions et le montant moyen des transactions, ainsi qu'entre les taux d'intérêt et le montant moyen des transactions, indique des changements dans le marché qui méritent une attention particulière. Ces changements pourraient être dus à des facteurs macroéconomiques, des modifications de la réglementation immobilière, ou des variations dans l'offre et la demande de logements.
 
-    Nous espérons que cette application vous a fourni des insights précieux sur le marché immobilier et vous encourageons à continuer vos explorations et analyses.
+        Pour les utilisateurs de cette application, il est recommandé de continuer à surveiller ces tendances et d'approfondir l'analyse pour comprendre les dynamiques sous-jacentes qui influencent le marché immobilier français. En utilisant cette application, vous disposez d'un outil puissant pour explorer les données et prendre des décisions informées.
 
-    Bonnes analyses et merci de votre utilisation de notre application !
-    """)
+        Nous espérons que cette application vous a fourni des insights précieux sur le marché immobilier et vous encourageons à continuer vos explorations et analyses.
+
+        Bonnes analyses et merci de votre utilisation de notre application !
+        """)
+
+    # Image à droite
+    with col2:
+        st.image("maison.png", caption="Marché immobilier en France", use_column_width=True)
+
--- src/views/home.py
+++ src/views/home.py
@@ -3,7 +3,7 @@ from src.router import redirect
 
 def load_view():
 
-    st.title("Présentation de l'Application d'Analyse du Marché Immobilier en France")
+    st.title("Présentation de l'application d'analyse du marché immobilier en France")
 
     # Créer un tableau à trois colonnes
     col1, col2, col3 = st.columns([3, 5, 1])  # La colonne du milieu est plus large pour l'image
@@ -12,42 +12,41 @@ def load_view():
     with col2:
         st.image("house.jpg", width=500)  # Ajustez 'width' pour réduire la taille de l'image
 
-    st.write("""
-    ### Objectif de l'Application
+    st.markdown("""
+    ### Objectif de l'application
     Le principal objectif de cette application est de mettre en évidence les incohérences de marché entre le prix de l'immobilier et les taux d'intérêt. En explorant les données, vous pourrez identifier des tendances, des anomalies et potentiellement des opportunités sur le marché immobilier français.
 
-    ### Sources des Données
-    - **Transactions immobilières :** Les données proviennent de la base "Demande de Valeurs Foncières" (DVF), qui recense les transactions immobilières en France.
-    - **Taux d'intérêt :** Les données des taux d'intérêt sont fournies par la Banque de France.
+    ### Sources des données
+    - **Transactions immobilières :** les données proviennent de la base "Demande de Valeurs Foncières" (DVF), qui recense les transactions immobilières en France.
+    - **Taux d'intérêt :** les données des taux d'intérêt sont fournies par la Banque de France.
 
-    ### Visualisations Disponibles
+    ### Visualisations disponibles
     L'application propose trois graphiques principaux, chacun permettant de sélectionner la région de votre choix pour une analyse plus précise :
 
-    1. **Nombre de Transactions et Taux d'Intérêt Moyen par Mois :**
+    1. **Nombre de transactions et taux d'intérêt moyen par mois :**
     - Ce graphique montre le nombre de transactions immobilières et le taux d'intérêt moyen mensuel. 
     - Il permet d'observer comment les variations des taux d'intérêt peuvent affecter le volume des transactions.
 
-    2. **Nombre de Transactions et Montant Moyen des Transactions par Mois :**
+    2. **Nombre de transactions et montant moyen des transactions par mois :**
     - Ce graphique affiche le nombre de transactions et le montant moyen des transactions chaque mois.
     - Vous pouvez explorer les fluctuations des prix des biens immobiliers en relation avec le volume des transactions.
 
-    3. **Montant Moyen des Transactions et Taux d'Intérêt Moyen par Mois :**
+    3. **Montant moyen des transactions et taux d'intérêt moyen par mois :**
     - Ce graphique met en relation le montant moyen des transactions immobilières avec le taux d'intérêt moyen.
     - Il permet de visualiser les éventuelles corrélations entre les prix des transactions et les taux d'intérêt.
 
-    ### Technologies Utilisées
-    - **Streamlit :** L'application a été créée avec Streamlit, une bibliothèque Python permettant de créer des applications web interactives pour les analyses de données.
-    - **Pandas :** Les données sont manipulées et analysées à l'aide de Pandas, une bibliothèque Python puissante pour la manipulation des données.
-    - **Matplotlib :** Les visualisations graphiques sont générées avec Matplotlib, une bibliothèque Python dédiée à la création de graphiques.
+    ### Technologies utilisées
+    - **Streamlit :** l'application a été créée avec Streamlit, une bibliothèque Python permettant de créer des applications web interactives pour les analyses de données.
+    - **Pandas :** les données sont manipulées et analysées à l'aide de Pandas, une bibliothèque Python puissante pour la manipulation des données.
+    - **Matplotlib :** les visualisations graphiques sont générées avec Matplotlib, une bibliothèque Python dédiée à la création de graphiques.
 
-    ### Utilisation de l'Application
+    ### Utilisation de l'application
     L'interface utilisateur est conçue pour être simple et intuitive. Vous pouvez sélectionner la région de votre choix à l'aide des menus déroulants et observer instantanément les changements dans les graphiques. Chaque graphique offre des perspectives uniques sur le marché immobilier, permettant une analyse approfondie des tendances et des anomalies.
 
-    ### Limites et Avertissements
+    ### Limites et avertissements
     Les données présentées sont basées sur les transactions enregistrées et les taux publiés par les sources mentionnées. Les utilisateurs doivent garder à l'esprit que d'autres facteurs non représentés dans ces visualisations peuvent également influencer le marché immobilier. Les analyses fournies par cette application doivent être complétées par des recherches supplémentaires pour une prise de décision éclairée.
 
     Nous espérons que cette application vous sera utile pour mieux comprendre le marché immobilier en France et pour identifier des tendances et des incohérences importantes. N'hésitez pas à explorer les différentes régions et à tirer vos propres conclusions à partir des données présentées.
 
     Bonnes analyses !
     """)
-
